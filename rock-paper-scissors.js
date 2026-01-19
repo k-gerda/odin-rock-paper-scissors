1401559f6a8da38de137b2ac325cde91187383b3
zsiri93 <234030901+zsiri93@users.noreply.github.com>
2026-01-19 09:06:29 +0100

    Add a new function named playGame.     Move playRound function so that it is declared inside of the new playGame function.     Play 5 rounds by calling playRound 5 times.
    Fix the getComputerChoice function.
        Move the randomNumber variable inside the getComputerChoice function so that it creates a new random number in every round.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,14 +3,12 @@
     
     // Generate random numbers between 0 and 2. 
     // Round them down to integers.
-
-let randomNumber = Math.floor(Math.random()*3);
-
     // If the current number is 0 return rock.
     // If the current number is 1 return paper.
     // If the current number is 2 return scissor.
 
 function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random()*3);
     if (randomNumber===0) {
         return "rock";
     } else if (randomNumber===1) {
@@ -28,7 +26,7 @@ function getComputerChoice () {
 function getHumanChoice () {
    let humanSelection = prompt("Select rock, paper or scissor!").toLowerCase(); // Make it case-insensitive.
    if (humanSelection==="rock") {
-    return "rock"
+    return "rock";    
    } else if (humanSelection==="paper") {
     return "paper"
    } else if (humanSelection==="scissor") {
@@ -48,35 +46,68 @@ let computerScore = 0;
 // Write the logic to play a single round.
     // Create a new function named playRound.
     // Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-    // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
+    // Write the code for playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
     // Increment the humanScore or computerScore variable based on the round winner.
 
-function playRound (humanChoice,computerChoice) {
+// Write the logic to play the entire game
+    // Create a new function named playGame.
+    // Move playRound function and score variables so that they’re declared inside of the new playGame function
+    // Play 5 rounds by calling playRound 5 times. 
 
-    if (humanChoice ==="rock" && computerChoice === "scissor") {
-        console.log("You win! Rock beats scissor.")
-        humanScore++;
-    } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("You lose! Paper beats rock.")
-        computerScore++;
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("You win! Paper beats rock.")
-        humanScore++;
-    } else if (humanChoice === "paper" && computerChoice === "scissor") {
-        console.log("You lose! Scissor beats paper.")
-        computerScore++;
-    } else if (humanChoice === "scissor" && computerChoice === "paper") {
-        console.log("You win! Scissor beats paper.")
-        humanScore++;
-    } else if (humanChoice === "scissor" && computerChoice === "rock") {
-        console.log("You lose! Rock beats scissor.")
-        computerScore++
+
+function playGame () {
+    
+    let humanAnswer = getHumanChoice();
+    let computerAnswer = getComputerChoice();
+    playRound(humanAnswer, computerAnswer);
+        
+    function playRound (humanChoice,computerChoice) {
+                  
+        if (humanChoice ==="rock" && computerChoice === "scissor") {
+            console.log("You win! Rock beats scissor.")
+            humanScore++;
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log("You lose! Paper beats rock.")
+            computerScore++;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log("You win! Paper beats rock.")
+            humanScore++;
+        } else if (humanChoice === "paper" && computerChoice === "scissor") {
+            console.log("You lose! Scissor beats paper.")
+            computerScore++;
+        } else if (humanChoice === "scissor" && computerChoice === "paper") {
+            console.log("You win! Scissor beats paper.")
+            humanScore++;
+        } else if (humanChoice === "scissor" && computerChoice === "rock") {
+            console.log("You lose! Rock beats scissor.")
+            computerScore++
+        } else {
+            console.log("Draw!")
+        };
+        console.log("Your score: " + humanScore);
+        console.log("Computer score: " + computerScore);
+
+};    
+  
+} 
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
+
+// Write a function that returns a message when the game ends.
+
+function gameOver () {
+    if (humanScore > computerScore) {
+        console.log("Congratulation, you win!");
+    } else if (humanScore < computerScore) {
+        console.log("Game over! You lose!");
     } else {
-        console.log("Drawn game!")
+        console.log("Draw game!");
     }
+
 }
 
-let humanAnswer = getHumanChoice();
-let computerAnswer = getComputerChoice();
-
-playRound(humanAnswer,computerAnswer);
+gameOver();
